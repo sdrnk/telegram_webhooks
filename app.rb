edc2a8ada2268b51e23e609dc5fe37bc43cd8806
sdrnk <espozito89@gmail.com>
2018-11-10 14:50:08 +0200

    Inspectlet webhook to telegram

--- app.rb
+++ app.rb
@@ -12,3 +12,13 @@ post '/webhook_sentry' do
   message = "New exception detected by Sentry:\n*NoMethodError — #{error}*\n[View exception details on Sentry](#{url})"
   api.sendMessage(chat_id, message, {"parse_mode" => "Markdown"})
 end
+
+post '/webhook_inspectlet' do
+  token = ENV["TELEGRAM_TOKEN"]
+  chat_id = ENV["TELEGRAM_CHAT_ID"]
+  api = TelegramAPI.new token
+  hash = eval(request.params["payload"])
+  url = hash[:new_session][:sessionlink]
+  message = "New session is recorded by Inspectlet\n[View session on Inspectlet](#{url})"
+  api.sendMessage(chat_id, message, {"parse_mode" => "Markdown"})
+end
