7d36b9b9ec91d59df811dda28e559a9f9233f20e
sdrnk <espozito89@gmail.com>
2018-11-11 15:29:09 +0200

    add other info fields

--- app.rb
+++ app.rb
@@ -19,6 +19,11 @@ post '/webhook_inspectlet' do
   api = TelegramAPI.new token
   hash = eval(request.params["payload"])
   url = hash[:new_session][:sessionlink]
-  message = "New session is recorded by Inspectlet\n[View session on Inspectlet](#{url})"
+  ip = hash[:new_session][:ip]
+  country = hash[:new_session][:country]
+  landing_page = hash[:new_session][:landing_page]
+  session_duration = hash[:new_session][:session_duration]
+  duration = session_duration/1000
+  message = " New session is recorded by Inspectlet:\n #{ip} (#{country})\n #{landing_page}\n Session duration: #{duration} sec.\n [View session on Inspectlet](#{url})"
   api.sendMessage(chat_id, message, {"parse_mode" => "Markdown"})
 end
